e9655f2e684613864df1ae10d314bdef3a7ef8f6
Shingo Tamaki <shingo.tamaki@mercari.com>
2020-08-08 18:34:53 +0900

    feat: Add file for configurations API

---
+++ lib/testrail_api_client/api/configurations.rb
@@ -0,0 +1,11 @@
+module TestRail
+    class Client
+      # Methods for the Milestones API
+      #
+      # @see https://www.gurock.com/testrail/docs/api/reference/configurations
+      module Milestones
+        # TODO: implement
+      end
+    end
+  end
+  
