7fe6a6f83777e2934efd03bf868ab4f260a91af3
Shingo Tamaki <shingo.tamaki@mercari.com>
2020-08-09 18:15:18 +0900

    fix add some api file

--- lib/testrail_api_client/api.rb
+++ lib/testrail_api_client/api.rb
@@ -1,35 +1,43 @@
-# require 'testrail_api_client/api/case_fields'
-# require 'testrail_api_client/api/case_types'
-# require 'testrail_api_client/api/cases'
-# require 'testrail_api_client/api/milestones'
-# require 'testrail_api_client/api/plans'
-# require 'testrail_api_client/api/priorities'
+require 'testrail_api_client/api/attachments'
+require 'testrail_api_client/api/case_fields'
+require 'testrail_api_client/api/case_types'
+require 'testrail_api_client/api/cases'
+require 'testrail_api_client/api/configurations'
+require 'testrail_api_client/api/milestones'
+require 'testrail_api_client/api/plans'
+require 'testrail_api_client/api/priorities'
 require 'testrail_api_client/api/projects'
-# require 'testrail_api_client/api/result_fields'
-# require 'testrail_api_client/api/results'
-# require 'testrail_api_client/api/runs'
-# require 'testrail_api_client/api/sections'
-# require 'testrail_api_client/api/statuses'
-# require 'testrail_api_client/api/suites'
-# require 'testrail_api_client/api/tests'
-# require 'testrail_api_client/api/users'
+require 'testrail_api_client/api/reports'
+require 'testrail_api_client/api/result_fields'
+require 'testrail_api_client/api/results'
+require 'testrail_api_client/api/runs'
+require 'testrail_api_client/api/sections'
+require 'testrail_api_client/api/statuses'
+require 'testrail_api_client/api/suites'
+require 'testrail_api_client/api/templates'
+require 'testrail_api_client/api/tests'
+require 'testrail_api_client/api/users'
 
 module TestRailApiClient
     module API
-    #   include TestRailApiClient::CaseFields
-    #   include TestRailApiClient::CaseTypes
-    #   include TestRailApiClient::Cases
-    #   include TestRailApiClient::Milestones
-    #   include TestRailApiClient::Plans
-    #   include TestRailApiClient::Priorities
+        include TestRailApiClient::Attachments
+        include TestRailApiClient::CaseFields
+        include TestRailApiClient::CaseTypes
+        include TestRailApiClient::Cases
+        include TestRailApiClient::Configurations
+        include TestRailApiClient::Milestones
+        include TestRailApiClient::Plans
+        include TestRailApiClient::Priorities
         include TestRailApiClient::Projects
-    #   include TestRailApiClient::ResultFields
-    #   include TestRailApiClient::Results
-    #   include TestRailApiClient::Runs
-    #   include TestRailApiClient::Sections
-    #   include TestRailApiClient::Statuses
-    #   include TestRailApiClient::Suites
-    #   include TestRailApiClient::Tests
-    #   include TestRailApiClient::Users
+        include TestRailApiClient::Reports
+        include TestRailApiClient::ResultFields
+        include TestRailApiClient::Results
+        include TestRailApiClient::Runs
+        include TestRailApiClient::Sections
+        include TestRailApiClient::Statuses
+        include TestRailApiClient::Suites
+        include TestRailApiClient::Templates
+        include TestRailApiClient::Tests
+        include TestRailApiClient::Users
     end
 end
--- lib/testrail_api_client/api/templates.rb
+++ lib/testrail_api_client/api/templates.rb
@@ -1,8 +1,8 @@
 module TestRailApiClient
     # Methods for the Result Fields API
     #
-    # @see https://www.gurock.com/testrail/docs/api/reference/results-fields
-    module ResultFields
+    # @see https://www.gurock.com/testrail/docs/api/reference/templates
+    module Templates
         def get_result_fields
             # TODO
         end
