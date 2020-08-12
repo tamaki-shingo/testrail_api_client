b5c489596b6170744ab198274f7f7fd6c63b442c
Shingo Tamaki <shingo.tamaki@mercari.com>
2020-08-12 23:54:52 +0900

    feat: supports configurations API

--- lib/testrail_api_client/api/configurations.rb
+++ lib/testrail_api_client/api/configurations.rb
@@ -3,26 +3,40 @@ module TestRailApiClient
     #
     # @see https://www.gurock.com/testrail/docs/api/reference/configurations
     module Configurations
-        def get_configs
-            # TODO    
+        def configs(project_id)
+            get("get_configs/#{project_id}")  
         end
-        def add_config_group
-            # TODO    
+
+        # Paramters
+        # name	string	The name of the configuration group (required)
+        def add_config_group(project_id, name)
+            post("add_config_group/#{project_id}", {"name": name})
         end
-        def add_config
-            # TODO    
+
+        # Parameters
+        # name	string	The name of the configuration (required)
+        def add_config(config_group_id, name)
+            post("add_config/#{config_group_id}", {"name": name})
         end
-        def update_config_group
-            # TODO    
+
+        # Parameters
+        # name	string	The name of the configuration (required)
+        def update_config_group(config_group_id, name)
+            post("update_config_group/#{config_group_id}", {"name": name})
         end
-        def update_config
-            # TODO    
+        
+        # Parameters
+        # name	string	The name of the configuration (required)
+        def update_config(config_id, name)
+            post("update_config/#{config_id}", {"name": name})
         end
-        def delete_config_group
-            # TODO    
+
+        def delete_config_group(config_group_id)
+            post("delete_config_group/#{config_group_id}")
         end
-        def delete_config
-            # TODO    
+
+        def delete_config(config_id)
+            post("delete_config/#{config_id}")
         end
     end
 end
