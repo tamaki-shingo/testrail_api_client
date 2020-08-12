7ddd57142e1875b159e8c093bc2e47e6f067a5bc
Shingo Tamaki <shingo.tamaki@mercari.com>
2020-08-13 03:14:59 +0900

    feat: supports attachments API

--- lib/testrail_api_client/api/attachments.rb
+++ lib/testrail_api_client/api/attachments.rb
@@ -3,38 +3,48 @@ module TestRailApiClient
     #
     # @see https://www.gurock.com/testrail/docs/api/reference/attachments
     module Attachments
-        def  add_attachment_to_plan
-        # TODO
+        def add_attachment_to_plan(plan_id, path)
+            post("add_attachment_to_plan/#{plan_id}", path)
         end
-        def  add_attachment_to_plan_entry
-        # TODO
+
+        def add_attachment_to_plan_entry(plan_id, entry_id, path)
+            post("add_attachment_to_plan_entry/#{plan_id}/#{entry_id}", path)
         end
-        def  add_attachment_to_result
-        # TODO
+        
+        def add_attachment_to_result(result_id, path)
+            post("add_attachment_to_result/#{result_id}", path)
         end
-        def  add_attachment_to_run
-        # TODO
+        
+        def add_attachment_to_run(run_id, path)
+            post("add_attachment_to_run/#{run_id}", path)
         end
-        def  get_attachments_for_case
-        # TODO
+        
+        def get_attachments_for_case(case_id)
+            get("get_attachments_for_case/#{case_id}")
         end
-        def  get_attachments_for_plan
-        # TODO
+        
+        def get_attachments_for_plan(plan_id)
+            get("get_attachments_for_plan/#{plan_id}")
         end
-        def  get_attachments_for_plan_entry
-        # TODO
+        
+        def get_attachments_for_plan_entry(plan_id, entry_id)
+            get("get_attachments_for_plan_entry/#{plan_id}/#{entry_id}")
         end
-        def  get_attachments_for_run
-        # TODO
+        
+        def get_attachments_for_run(run_id)
+            get("get_attachments_for_run/#{run_id}")
         end
-        def  get_attachments_for_test
-        # TODO
+        
+        def get_attachments_for_test(test_id)
+            get("get_attachments_for_test/#{test_id}")
         end
-        def  get_attachment
-        # TODO
+        
+        def get_attachment(attachment_id)
+            get("get_attachment/#{attachment_id}")
         end
-        def  delete_attachment
-        # TODO
+        
+        def delete_attachment(attachment_id)
+            post("delete_attachment/#{attachment_id}")
         end
     end
 end
