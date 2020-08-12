7b2f4b1820274ac86379ad674ce2b86951ee1870
Shingo Tamaki <shingo.tamaki@mercari.com>
2020-08-12 20:41:19 +0900

    feat: supports add_milestone

--- lib/testrail_api_client/api/milestones.rb
+++ lib/testrail_api_client/api/milestones.rb
@@ -11,8 +11,16 @@ module TestRailApiClient
             get("get_milestones/#{project_id}")
         end
 
-        def add_milestone
-            # TODO
+        # Parameters
+        # Name          Type        Description
+        # name          string      The name of the milestone (required)
+        # description   string      The description of the milestone
+        # due_on        timestamp   The due date of the milestone (as UNIX timestamp)
+        # parent_id     int         The ID of the parent milestone, if any (for sub-milestones) (available since TestRail 5.3)
+        # refs          string      A comma-separated list of references/requirements (available since TestRail 6.4)
+        # start_on      timestamp   The scheduled start date of the milestone (as UNIX timestamp) (available since TestRail 5.3)
+        def add_milestone(project_id, opt)
+            post("add_milestone/#{project_id}", opt)
         end
 
         def update_milestone
