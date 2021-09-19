88f5f0fbc963f4b5f640a6636a28c092c33cab03
Shingo Tamaki <shingo.tamaki@mercari.com>
2021-09-19 18:42:06 +0900

    fix: Update tests for Case Fields API

--- spec/case_fields_spec.rb
+++ spec/case_fields_spec.rb
@@ -9,10 +9,37 @@ RSpec.describe TestRailApiClient do
   client = TestRailApiClient::Client.new(url, user, pass)
 
   context 'Case Fields' do
-    it 'can get test case fields' do
+    it 'can get case custom fields' do
       fields = client.case_fields
       # Response is depends on your setting
       expect(fields).not_to be nil
+    end
+    it 'can add case custom field' do
+      # It have been able to be manually verified the behavior of this API, 
+      # but since the deletion API is not supported, It have be excluded it from the unit test.
+
+      # opt = {
+      #   "type": 'Multiselect',
+      #   "name": 'tmk_multiselect',
+      #   "label": 'TMK Multiselect',
+      #   "description": 'my custom Multiselect description',
+      #   "configs": [
+      #     {
+      #       "context": {
+      #         "is_global": false,
+      #         "project_ids": [
+      #           38
+      #         ]
+      #       },
+      #       "options": {
+      #         "is_required": false,
+      #         "items": "1, One\n2, Two"
+      #       }
+      #     }
+      #   ],
+      #   "include_all": true
+      # }
+      # client.add_case_field(opt)
     end
   end
 end
