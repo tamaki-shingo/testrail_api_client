8b822ada4d3e44fcd7541fed8a7a7c179f8cc833
Shingo Tamaki <shingo.tamaki@mercari.com>
2021-09-19 14:47:26 +0900

    fix: Fix wrong url link

--- lib/testrail_api_client/api/case_fields.rb
+++ lib/testrail_api_client/api/case_fields.rb
@@ -4,7 +4,7 @@ module TestRailApiClient
   # Methods for the Case Fields API
   #
   # Use the following API methods to request details about custom fields for test cases.
-  # @see https://www.gurock.com/testrail/docs/api/reference/cases-fields
+  # @see https://www.gurock.com/testrail/docs/api/reference/case-fields
   module CaseFields
     def case_fields
       get('get_case_fields')
